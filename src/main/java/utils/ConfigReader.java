7ee9c8fbe5a926af74a86211f22e017c9c827ec6
Miłosz Jura <milosz.m.jura@gmail.com>
2025-03-07 18:55:25 +0100

    Added ConfigReader

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,13 +1,24 @@
-package test;
+package utils;
 
-/**
- * Hello world!
- *
- */
-public class ConfigReader
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class ConfigReader{
+	private static Properties properties = new Properties();
+	static {
+		try {
+			FileInputStream fileInput = new FileInputStream("src/test/resources/config.properties");
+            properties.load(fileInput);
+
+		} catch(IOException e){
+			//throw new RuntimeException("Failed to load configuration file: " + e.getMessage());
+			System.err.println("Failed to load configuration file");
+		}
+	}
+
+	public static String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 }
