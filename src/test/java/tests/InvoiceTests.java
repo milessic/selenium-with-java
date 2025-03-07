56ddd5393f227a66a3245e892a26055a8c9e94aa
Miłosz Jura <milosz.m.jura@gmail.com>
2025-03-07 18:55:36 +0100

    Added Test Cases for checking visibility of elements on the page

--- src/test/java/tests/InvoiceTests.java
+++ src/test/java/tests/InvoiceTests.java
@@ -1,38 +1,74 @@
-package test;
+package tests;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import pages.InvoicePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class InvoiceTests
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	private WebDriver driver;
+	private InvoicePage invoicePage;
+
+
+	@BeforeEach
+	public void testSetup(){
+		driver = new ChromeDriver();
+		invoicePage = new InvoicePage(driver);
+
+		invoicePage.openInvoicePage();
+	}
+
+	@AfterEach
+	public void testTeardown(){
+		invoicePage.closePage();
+	}
+	@Test
+	public void testOpenPage(){
+		assertEquals("Generator Faktur VAT",invoicePage.getTitle());
+	}
+
+	@Test
+	public void testAllDefaultFormElementsAreDisplayedOnPageLoad(){
+		// TopNav
+		invoicePage.verifyThatAllTopNavElementsAreVisible();
+		// Footer
+		invoicePage.verifyThatAllFooterElementsAreVisible();
+		// Form fields
+		invoicePage.verifyThatAllFormElementsAreVisible();
+		// Corrections - not visible
+		invoicePage.verifyThatCorrectionFieldAreNotVisible();
+	}
+
+	@Test
+	public void testThatOneRowIsGeneratedOnLoad(){
+		invoicePage.verifyThatOneInvoiceRowIsGeneratedByDefault();
+	}
 
     /**
      * @return the suite of tests being tested
      */
+	/*
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
+	*/
 
     /**
      * Rigourous Test :-)
      */
+	/*
     public void testApp()
     {
         assertTrue( true );
     }
+	*/
 }
