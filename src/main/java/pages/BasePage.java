da6ace1785ebdadf6191538a544531944838f83f
Miłosz Jura <milosz.m.jura@gmail.com>
2025-03-07 18:45:14 +0100

    Added BasePage methods

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,13 +1,65 @@
-package test;
+package pages;
 
-/**
- * Hello world!
- *
- */
-public class BasePage
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.By;
+
+public class BasePage {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+
+	public BasePage(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+	}
+
+	protected boolean isElementDisplayed(WebElement element){
+		try{
+			return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	protected void click(WebElement element){
+		wait.until(ExpectedConditions.visibilityOf(element)).click();
+	}
+
+	public void closePage(){
+		driver.close();
+	}
+
+	protected String getText(WebElement element){
+		return wait.until(ExpectedConditions.visibilityOf(element)).getText();
+	}
+
+	protected void fillText( WebElement element, String text ) {
+		wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(text);
+	}
+
+	public String getTitle(){
+		return driver.getTitle();
+	}
+	public void waitUntilElementByIdIsInvisible(String elementId){
+		WebElement element = driver.findElement(By.id(elementId));
+		wait.until(ExpectedConditions.invisibilityOf(element));
+	}
+
+	public void waitUntilElementByByIsVisible(By by){
+		WebElement element = driver.findElement(by);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void waitUntilElementByIdIsVisible(String elementId){
+		WebElement element = driver.findElement(By.id(elementId));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void waitUntilElementByXpathIsVisible(String elementXpath){
+		WebElement element = driver.findElement(By.xpath(elementXpath));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
 }
