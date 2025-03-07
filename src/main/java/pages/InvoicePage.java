41514d0b3acc94dddcd88bde75a8d0746469ccf0
Miłosz Jura <milosz.m.jura@gmail.com>
2025-03-07 18:54:56 +0100

    Added InvoicePage class and methods to check visibility of elements

---
+++ src/main/java/pages/InvoicePage.java
@@ -0,0 +1,178 @@
+package pages;
+
+import utils.ConfigReader;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.Map;
+
+
+public class InvoicePage extends BasePage{
+	String [] xpathsOfTopBarElements = {
+		"//h2[text()='Generator Faktur']",
+		"//button[@id='btn-pdf']",
+		"//p[@id='pdf-status']",
+		"//button[@id='show-example']",
+	};
+
+	String [] xpathsOfFooterElements = {
+		"//button[@id='importJSON']",
+		"//button[@id='exportJSON']",
+		"//a[href='https://github.com/milessic/fv' and text()='fv on GitHub']"
+	};
+
+	String [] idsOfCorrectionFields = {
+		"correctionReason",
+		"correctedDocument",
+	};
+
+	String [] idsOfInvoiceHeaders = {
+		"isMetodaKasowa",
+		"isCorrection",
+		"invoiceNumber",
+		"invoiceDate",
+		"dateOfSales",
+		"paymentTerm",
+		"paymentMethod",
+		"bankAccountNumber",
+		"authorizedPerson",
+		"note",
+	};
+
+	String [] idsOfSeller = {
+		"fromName",
+		"fromName2",
+		"fromStreet",
+		"fromCity",
+		"fromNip",
+		"fromRegon",
+		"fromKrs",
+		"fromCountry"
+	};
+
+	String [] idsOfBuyer = {
+		"toName",
+		"toName2",
+		"toStreet",
+		"toCity",
+		"toNip",
+		"toKrs",
+		"toCountry"
+		};
+	private final By isMetodaKasowa = By.id("isMetodaKasowa");
+	private final By isCorrection = By.id("isCorrection");
+	private final By invoiceNumber = By.id("invoiceNumber");
+	private final By invoiceDate = By.id("invoiceDate");
+	private final By dateOfSales = By.id("dateOfSales");
+	private final By paymentTerm = By.id("paymentTerm");
+	private final By paymentMethod = By.id("paymentMethod");
+	private final By bankAccountNumber = By.id("bankAccountNumber");
+
+	private final By correctionReason = By.id("correctionReason");
+	private final By correctedDocument = By.id("correctedDocument");
+	private final By authorizedPerson = By.id("authorizedPerson");
+	private final By note = By.id("note");
+
+	private final By fromName = By.id("fromName");
+	private final By fromName2 = By.id("fromName2");
+	private final By fromStreet = By.id("fromStreet");
+	private final By fromCity = By.id("fromCity");
+	private final By fromNip = By.id("fromNip");
+	private final By fromRegon = By.id("fromRegon");
+	private final By fromKrs = By.id("fromKrs");
+	private final By fromAddressCountry = By.id("fromAddressCountry");
+
+	private final By toName = By.id("toName");
+	private final By toName2 = By.id("toName2");
+	private final By toStreet = By.id("toStreet");
+	private final By toCity = By.id("toCity");
+	private final By toNip = By.id("toNip");
+	private final By toKrs = By.id("toKrs");
+	private final By toCountry = By.id("toCountry");
+
+
+
+	public InvoicePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public void openInvoicePage() {
+		String url = ConfigReader.getProperty("url");
+		driver.get(url);
+	}
+
+
+	public void fillFormFields(Map<String, String> formData){
+		for (Map.Entry<String, String> entry : formData.entrySet()){
+			WebElement element = driver.findElement(By.id(entry.getKey()));
+			wait.until(ExpectedConditions.visibilityOf(element));
+			fillText(element, entry.getValue());
+		}
+	}
+
+	public void verifyThatAllTopNavElementsAreVisible(){
+		System.out.println("-Checking TopNav section");
+		for ( String elementId : this.xpathsOfTopBarElements){
+			this.waitUntilElementByXpathIsVisible(elementId);
+		}
+	}
+
+	public void verifyThatAllFooterElementsAreVisible(){
+		System.out.println("-Checking Footer section");
+		for ( String elementId : this.xpathsOfFooterElements){
+			this.waitUntilElementByXpathIsVisible(elementId);
+		}
+	}
+
+	public void verifyThatAllFormElementsAreVisible(){
+		this.waitUntilElementByByIsVisible(isMetodaKasowa);
+		// Headers
+		System.out.println("-Checking Headers section");
+		for ( String elementId : this.idsOfInvoiceHeaders){
+			this.waitUntilElementByIdIsVisible(elementId);
+		}
+		// Seller
+		System.out.println("-Checking Seller section");
+		for ( String elementId : this.idsOfSeller){
+			this.waitUntilElementByIdIsVisible(elementId);
+		}
+		// Buyer
+		System.out.println("-Checking Buyer section");
+		for ( String elementId : this.idsOfBuyer){
+			this.waitUntilElementByIdIsVisible(elementId);
+		}
+		System.out.println("\tAll form fields verified properly!");
+
+	}
+
+
+	public void verifyThatCorrectionFieldAreNotVisible(){
+		System.out.println("-Checking that Correction fields are not displayed");
+		for ( String elementId : this.idsOfCorrectionFields ){
+			this.waitUntilElementByIdIsInvisible(elementId);
+		}
+		System.out.println("\tCorrection fields are not displayed");
+	}
+
+	public void verifyThatOneInvoiceRowIsGeneratedByDefault(){
+		verifyThatCountOfInvoiceRowsIs(1);
+	}
+
+	public void verifyThatCountOfInvoiceRowsIs(int numOfRows){
+		System.out.println("-Checking that " + numOfRows + " row is generated");
+		int numOfElements = this.getNumberOfVisibleRows();
+		if ( numOfElements != 1 ){
+			throw new RuntimeException("Found " + numOfElements + " rows, but expected 1!");
+		}
+		System.out.println("\t" + numOfRows +"  row is generatd");
+	}
+
+	public int getNumberOfVisibleRows(){
+		return driver.findElements(By.xpath("//tbody[@id='itemsTable']/tr")).size();
+	}
+
+}
